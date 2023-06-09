ef1813d4e10363ecb3388476c1a969334cc56ef5
vmarialuzm <vmarialuzm@gmail.com>
2023-06-09 10:45:24 -0500

    implementando widget en el fomulario

--- portafolio/formulario/forms.py
+++ portafolio/formulario/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 
 class ProyectoForm(forms.Form):
-    foto = forms.URLField()
-    titulo_proyecto = forms.CharField(max_length=200) 
-    descripcion_proyecto = forms.CharField(widget=forms.Textarea)
-    tags = forms.CharField(max_length=200)
-    url_github = forms.URLField()
+    foto = forms.URLField(widget=forms.TextInput(attrs={
+        "class": "form-control mb-3"
+    }))
+    titulo_proyecto = forms.CharField(max_length=200, widget=forms.TextInput(attrs={
+        "class": "form-control mb-3"
+    })) 
+    descripcion_proyecto = forms.CharField(widget=forms.Textarea(attrs={
+        "class": "form-control mb-3"
+    }))
+    tags = forms.CharField(max_length=200,widget=forms.TextInput(attrs={
+        "class": "form-control mb-3"
+    }))
+    url_github = forms.URLField(widget=forms.TextInput(attrs={
+        "class": "form-control mb-3"
+    }))
