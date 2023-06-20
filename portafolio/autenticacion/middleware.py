a943bbc9184739f87067964f2156e7ba9b5c528a
vmarialuzm <vmarialuzm@gmail.com>
2023-06-20 12:43:14 -0500

    guadardando las ip en la db

--- portafolio/autenticacion/middleware.py
+++ portafolio/autenticacion/middleware.py
@@ -1,13 +1,12 @@
 from ipware import get_client_ip
+from .models import IPAddress
 
-lista_ip = []
+
 def is_valid_ip(get_response):
     def middleware(request):
         ip, is_routable = get_client_ip(request)
         response = get_response(request)
-        print("ip",ip)
-        print("is_routable",is_routable)
-        lista_ip.append(ip)
-        print("lista_ip",lista_ip)
+        IPAddress.objects.create(ip_address=ip)
+
         return response
     return middleware
--- portafolio/autenticacion/models.py
+++ portafolio/autenticacion/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class IPAddress(models.Model):
+    ip_address = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = "ipaddress"
